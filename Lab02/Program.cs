7e29bebe2f4cc0845f274fcf4c16c8f897b68e34
ethan3212 <74456094+ethan3212@users.noreply.github.com>
2022-01-03 08:12:23 -0600

    Add files via upload

---
+++ Lab02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+namespace Lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int junk = Factorial2(5);
+            WriteLine("junk is:" + junk);
+            ReadLine();
+        }
+
+        static int Factorial(int N)
+        {
+            if (N == 1)
+                return N;
+            else
+            {
+                int smaller = N - 1;
+                int answer = Factorial(smaller);
+                return (N * answer);
+            }
+        }
+        static int Factorial2(int N)
+        {
+            int answer = N;
+
+            for(int i=N-1; i>1; i--)
+            {
+                answer = answer * i;
+            }
+
+            return answer;
+        }
+    }
+}
